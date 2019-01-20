2279fe501171387c88ebf0d1803c0a0255b55139
Jón Bjarni Bjarnason <jbbjarnason@marel.com>
2019-01-20 21:06:26 +0000

    #noissue depending on appbase, not compiling

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 //
 // Created by jonb on 20.1.2019.
 //
+#include <appbase/application.hpp>
+#include "inc/RaspberryPiPorts.h"
+#include <iostream>
 
-int main()
-{
+
+int main( int argc, char** argv ) {
+    try {
+        appbase::app().register_plugin<RaspberryPiPorts>();
+        if( !appbase::app().initialize_plugins( argc, argv ) )
+            return -1;
+        appbase::app().startup();
+        appbase::app().exec();
+    } catch ( const boost::exception& e ) {
+        std::cerr << boost::diagnostic_information(e) << "\n";
+    } catch ( const std::exception& e ) {
+        std::cerr << e.what() << "\n";
+    } catch ( ... ) {
+        std::cerr << "unknown exception\n";
+    }
+    std::cout << "exited cleanly\n";
     return 0;
 }
